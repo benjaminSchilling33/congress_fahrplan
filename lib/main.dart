1aaaf07c2095142fa2ba2a8b231f5d47b5a25ec9
Benjamin Schilling <benjamin.a.schilling@freenet.de>
2019-11-16 23:29:29 +0100

    upgrade to android x and latest package versions

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ SPDX-License-Identifier: GPL-2.0-only
 Copyright (C) 2019 Benjamin Schilling
 */
 
-import 'package:connectivity/connectivity.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
